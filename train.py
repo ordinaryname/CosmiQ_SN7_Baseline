dba54fb61875383f1c6bfdf8c53d5f0cc4a9a9d2
Daniel Hogan <dhogan@iqt.org>
2020-09-23 12:16:00 -0400

    Root dir python programs to set file paths

---
+++ train.py
@@ -0,0 +1,16 @@
+import solaris as sol
+import sys
+import os
+config_path = 'yml/sn7_baseline_train.yml'
+config = sol.utils.config.parse(config_path)
+config['training_data_csv'] = sys.argv[1]
+config['training']['callbacks']['model_checkpoint']['filepath'] = os.path.abspath('./models/sn7_baseline/xdxd_best.pth')
+config['training']['model_dest_path'] = os.path.abspath('./models/sn7_baseline/xdxd_final.pth')
+print('Config:')
+print(config)
+
+# make model output dir
+os.makedirs(os.path.dirname(config['training']['model_dest_path']), exist_ok=True)
+
+trainer = sol.nets.train.Trainer(config=config)
+trainer.train()
--- train.sh
+++ train.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 echo Train
+cd "$0"/src
+time python sn7_data_prep.py $1
+time python sn7_baseline_train.py $1
