779e4461ffd23058ddc05613d8d1ab72ebc049a4
Daniel Hogan <dhogan@iqt.org>
2020-09-23 20:04:52 -0400

    Fixed manually-set paths after reading yaml files

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import solaris as sol
 import os
 config_path = 'yml/sn7_baseline_infer.yml'
 config = sol.utils.config.parse(config_path)
+config['model_path'] = os.path.abspath('./models/sn7_baseline/xdxd_final.pth')
+config['inference_data_csv'] = os.path.abspath('./output/csvs/sn7_baseline_test_df.csv') 
+config['inference']['output_dir'] = os.path.abspath('./inference_out/sn7_baseline_preds/raw')
 print('Config:')
 print(config)
 
